f52d2401f75e484248900d4b0f9af23746c96dbe
RupalMehta5 <64820046+RupalMehta5@users.noreply.github.com>
2022-04-10 20:02:36 +0530

    Add files via upload

---
+++ Python/Activities/Activity20.py
@@ -0,0 +1,11 @@
+import pandas
+dataframe = pandas.read_excel('C:\\Rupal\\IBM Training SDET\\SDET PythonFolder\\Activity19.xlsx')
+print(dataframe)
+print("===============================")
+print("Number of rows and columns:", dataframe.shape)
+print("==========================")
+print("Emails:")
+print(dataframe['Email'])
+print("===============================")
+print("Sorted data")
+print(dataframe.sort_values('FirstName'))
