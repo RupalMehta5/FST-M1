60063402f9d06c3349d1773018ea5b92fb73aea5
RupalMehta5 <64820046+RupalMehta5@users.noreply.github.com>
2022-04-18 17:05:52 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_DemoOne.java
@@ -0,0 +1,16 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+	@Test
+	public void firstTestcase() {
+		System.out.println("First test case for DemoOne Class");
+	}
+	
+	@Test
+	public void secondTestcase() {
+		System.out.println("Second test case for DemoOne Class");
+	}
+
+}
